5b91be45e90c29302586841c04c4e3f8b4a96f14
Li Haomin <lhmlihaomin@live.com>
2018-08-14 16:00:07 +0800

    it works.

--- qcloudupdate.py
+++ qcloudupdate.py
@@ -10,12 +10,23 @@ from scp import SCPClient
 
 from ssh import SshHandler
 
+
+def run_cmd(ssh_client, cmd):
+    print "----------------------------------------"
+    print "CMD:"
+    print "    "+cmd
+    exitcode, stdout, stderr = ssh_client.run(cmd)
+    print "OUTPUT:"
+    print stdout
+    print "----------------------------------------"
+
+
 # Local parameters:
 PACKAGE_DIR = ".\\packages"
 PEM_DIR = ".\\pem"
 
 # Module parameters:
-MODULE_NAME = "fakeservice"
+MODULE_NAME = "fakedjango"
 OLD_VERSION = "1.0.0"
 NEW_VERSION = "1.0.5"
 
@@ -41,8 +52,7 @@ ssh = SshHandler(host['ip'], host['username'], host['key_name'], PEM_DIR)
 
 print "Checking module path:"
 cmd = "ls {0}".format(module_path)
-exitcode, stdout, stderr = ssh.run(cmd)
-print stdout
+run_cmd(ssh, cmd)
 
 raw_input("Press Enter to continue ...")
 # copy new version package:
@@ -51,38 +61,47 @@ scp = SCPClient(ssh.client.get_transport())
 scp.put(package_file, module_path)
 
 cmd = "ls {0}".format(module_path)
-exitcode, stdout, stderr = ssh.run(cmd)
-print stdout
+run_cmd(ssh, cmd)
 
 raw_input("Press Enter to continue ...")
 # extract new version package:
 print "# Extracting package:"
 cmd = "tar -C {0} -xzf {1}/{2}".format(module_path, module_path, fname)
-print cmd
-exitcode, stdout, stderr = ssh.run(cmd)
+run_cmd(ssh, cmd)
 
 cmd = "ls {0}".format(module_path)
-exitcode, stdout, stderr = ssh.run(cmd)
-print stdout
+run_cmd(ssh, cmd)
+
+raw_input("Press Enter to continue ...")
+# modify autostart script:
+print "# Editing startup script ..."
+cmd = "sudo sed -i 's/{0}-{1}-{2}/{0}-{1}-{3}/g' /etc/rc.local".format(
+    'cloud', 
+    MODULE_NAME, 
+    OLD_VERSION, 
+    NEW_VERSION
+    )
+run_cmd(ssh, cmd)
 
 raw_input("Press Enter to continue ...")
 # stop old version service:
 print "# Stopping old version:"
 cmd = "cd {0}/bin&&./stop.sh".format(service_path_old)
-print cmd
-exitcode, stdout, stderr = ssh.run(cmd)
+run_cmd(ssh, cmd)
 
-cmd = "ps -ef|grep SimpleHTTPServer"
-exitcode, stdout, stderr = ssh.run(cmd)
-print stdout
+cmd = "curl localhost:12345/"
+run_cmd(ssh, cmd)
 
 raw_input("Press Enter to continue ...")
 # start new version service:
 print "# Starting new version:"
 cmd = "cd {0}/bin&&./start.sh".format(service_path_new)
-print cmd
-exitcode, stdout, stderr = ssh.run(cmd)
+run_cmd(ssh, cmd)
 
-cmd = "ps -ef|grep SimpleHTTPServer"
-exitcode, stdout, stderr = ssh.run(cmd)
-print stdout
+# check new version process:
+cmd = "curl localhost:12345/"
+run_cmd(ssh, cmd)
+
+# remove tarball:
+cmd = "rm {0}/{1}".format(module_path, fname)
+run_cmd(ssh, cmd)
